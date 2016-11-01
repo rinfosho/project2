37aef849d4ca4e83b090338e1bec5f21e0023693
Parintr Sachavirawongse <parintr96@hotmail.com>
2016-11-01 14:57:45 +0700

    Changes made (A little)

--- phase0_keepalive.py
+++ phase0_keepalive.py
@@ -51,39 +51,34 @@ def attack(url, numreq, maxCon):
     tottime=0
     for i in range(int(times)):
         for j in range(maxCon):
-            start=time.time()
             try:
+                start=time.time()
                 attack_socket.send(http_request)
+                end = time.time()
                 pass_counter+=1
-            except skt.error, e:
+                tottime += (end-start)
+            except (skt.timeout, skt.error):
                 fail_count +=1
-                continue
-            except skt.timeout:
-                fail_count += 1
-                continue
-            # while '\r\n\r\n' not in head:
-            #     buf = attack_socket.recv(1024)
-            #     head += buf
-            # content.append(head)
-            end = time.time()
-            tottime += (end-start)
+            continue
+            while '\r\n\r\n' not in head:
+                buf = attack_socket.recv(1024)
+                head += buf
+            content.append(head)
+            
     if (pass_counter + fail_count) != numreq:
         for k in range((numreq-(pass_counter + fail_count))):
-            start=time.time()
             try:
+                start=time.time()
                 attack_socket.send(http_request)
                 pass_counter+=1
-            except skt.timeout:
+                end = time.time()
+                tottime += end-start
+            except (skt.error, skt.timeout):
                 fail_count+=1
-            except skt.error, e:
-                fail_count +=1
-                continue
-            # while '\r\n\r\n' not in head:
-            #     buf = attack_socket.recv(1)
-            #     head += buf
-            # pass_counter +=1
-            end = time.time()
-            tottime += end-start
+            continue
+            while '\r\n\r\n' not in head:
+                buf = attack_socket.recv(1)
+                head += buf
 
     content_to_load = ""
     head =""
@@ -93,14 +88,14 @@ def attack(url, numreq, maxCon):
     header_data = head.split("\r\n\r\n")[0]
     attack_socket.close()
     # return pass_counter, fail_count, tottime
-    print "Replies from the server: ", header_data
+    print "Replies from the server: \r\n", header_data + "\r\n\r\n"
     print "Time taken for tests: ", tottime," seconds" 
     print "Completed requests: ", pass_counter
     print "Failed requests: ", fail_count
-    print "Avg requests per second: ", float(numreq)/tottime," [req/s]"
+    print "Avg requests per second: ", float(pass_counter)/tottime," [req/s]"
 
-url = "http://10.27.8.20:8080"
-numreq = 100
-maxCon = 20
+url = "http://10.27.8.20:8080/"
+numreq = 10000
+maxCon = 2000
 attack(url,numreq,maxCon)
 
